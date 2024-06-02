58049caf2336bcd64a259642be893912bc49e9e6
brunafb04 <brunaborges04@hotmail.com>
2024-06-02 18:47:12 -0400

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img src="./assets/logo bolos.jpeg" alt="logo bolos" sizes="small">
+  <HelloWorld msg="Marcia Bolos"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Times New Roman', Times, serif;
+  font-size: larger;
+  text-align: center;
+  color: black;
+  margin-top: 10px;
+  background-color: lightblue;
+
+}
+</style>
---
+++ HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+    Sejam bem vindos(as) a minha nova página
+    </p>
+    <h3>Modelos</h3>
+    <ul>
+      <li><a href="https://www.instagram.com/marciaferreirabolos/" target="_blank" rel="noopener">Referências</a></li>
+    </ul>
+    <h3>Faça seu pedido</h3>
+    <ul>
+      <li><a href="https://wa.me/5565999289914" target="_blank" rel="noopener">Orçamentos</a>
+      </li>
+    </ul>
+    <h3>Contato</h3>
+    <ul>
+      <li><a href="https://wa.me/5565999289914" target="_blank" rel="noopener">Whatsapp</a></li>
+      <li><a href="https://www.instagram.com/marciaferreirabolos/" target="_blank" rel="noopener">Instagram</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 30px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #dd8f99;
+}
+</style>
